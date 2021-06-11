3eedd059c30f306b6f0ef406b24ea09210c6224c
DhruthiGowda <85270517+DhruthiGowda@users.noreply.github.com>
2021-06-11 19:29:29 +0530

    Add files via upload

---
+++ ParticipantDal.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BalForEventMgt;
+
+namespace DalForEventMgt
+{
+    public class ParticipantDal
+    {
+        public bool authParticipant(string e="abc@gmail.com",string p="abc")
+        {
+            SqlConnection cn = new SqlConnection("server=DESKTOP-AT5MMP2\\SQLEXPRESS;Integrated Security=true;database=EventManagement");
+            SqlCommand cmd = new SqlCommand("select useremailid,password from participant ", cn);
+            cn.Open();
+            ParticipantLogin p1 = new ParticipantLogin();
+            
+            try
+            {
+                SqlDataReader dr = cmd.ExecuteReader();
+                string pwd = "";
+                if (dr.HasRows)
+                {
+                    dr.Read();
+                    p1.P_email = Convert.ToString(dr["useremailid"]);
+                    p1.P_password = Convert.ToString(dr["password"]);
+                    pwd = Convert.ToString(dr["password"]);
+                }
+                else
+                {
+                    return false;
+                }
+
+                cn.Close();
+
+                if (p == pwd)
+                {
+                    return true;
+
+                }
+                else
+                {
+                    return false;
+                }
+                
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+           
+            
+        }
+
+    }
+}
---
+++ ParticipantRegisterDal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BalForEventMgt;
+
+namespace DalForEventMgt
+{
+    public class ParticipantRegisterDal
+    {
+        public void authreg(Participant p)
+        {
+            Participant pl = new Participant();
+            SqlConnection cn = new SqlConnection("server=DESKTOP-AT5MMP2\\SQLEXPRESS;Integrated Security=true;database=EventManagement");
+            //SqlCommand cmd = new SqlCommand("insert into Participant values("p1.P_email","r1.P_password",r1.P_name,r1.P_contactnumber,r1.P_gender,r1.P_city")" , cn);
+            SqlCommand cmd = new SqlCommand("insert into Participant values(" + p.P_email + ",'" + p.P_password + "'," + p.P_name + ",'" + p.P_contactnumber + ",' " + p.P_gender + ",'" + p.P_city + " ')", cn);
+            cn.Open();
+
+            
+                cmd.ExecuteNonQuery();
+
+            cn.Close();
+            
+
+        }
+    }
+}
+            
+
+           
+           
+            
